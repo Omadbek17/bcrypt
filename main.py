54cc0e7ce815d0089f2c5542628b50ac40a9b094
omadbek <qosimoff06@gmail.com>
2024-04-24 09:35:13 +0500

    I added checking code hashing password

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ def hashing_password(raw_password):
     salt = bcrypt.gensalt(4)
     return bcrypt.hashpw(encoded_password, salt)
 
-print(hashing_password('123'))
+
+
+new_password = input('?: ').encode('utf-8')
+
+result = bcrypt.checkpw(new_password, hashing_password('123'))
+print(result)
